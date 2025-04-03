236370bb68a82af1d1f2ac51f7106741fb09b920
GabrielRoig10 <bielroig@gmail.com>
2025-04-03 10:45:10 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  }
+}
+
+module.exports = nextConfig
